fccb66fcc8882af8ed810d79430bf1ebd52afa14
Isaakavo <isaakhaas96@gmail.com>
2021-03-08 11:57:10 -0600

    Added calculations of totals for first and second half of the month

--- app/src/main/java/com/example/monthlyexpenses/HalfMonthTotals.kt
+++ app/src/main/java/com/example/monthlyexpenses/HalfMonthTotals.kt
@@ -7,18 +7,10 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val FIRST_HALF = "param1"
 private const val SECOND_HALF = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HalfMonthTotals.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HalfMonthTotals : DialogFragment() {
     // TODO: Rename and change types of parameters
     private var firstHalfValue: String? = null
@@ -58,16 +50,8 @@ class HalfMonthTotals : DialogFragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HalfMonthTotals.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
+
+      @JvmStatic
         fun newInstance(param1: String, param2: String) =
             HalfMonthTotals().apply {
                 arguments = Bundle().apply {
