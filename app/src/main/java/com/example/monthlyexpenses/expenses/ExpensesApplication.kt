8a0ac723f37fa3c9aa74835b686741bd405bff33
Isaakavo <isaakhaas96@gmail.com>
2021-03-22 16:50:50 -0600

    Timber added

--- app/src/main/java/com/example/monthlyexpenses/expenses/ExpensesApplication.kt
+++ app/src/main/java/com/example/monthlyexpenses/expenses/ExpensesApplication.kt
@@ -1,14 +1,25 @@
 package com.example.monthlyexpenses.expenses
 
 import android.app.Application
+import com.example.monthlyexpenses.BuildConfig
 import data.ExpensesRepository
 import data.ExpensesRoomDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
+import timber.log.Timber
 
 class ExpensesApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
     // No need to cancel this scope as it'll be torn down with the process
     val applicationScope = CoroutineScope(SupervisorJob())
+
     // Using by lazy so the database and the repository are only created when they're needed
     // rather than when the application starts
     val database by lazy { ExpensesRoomDatabase.getDatabase(this) }
