a415a566e63bd1040060cb9cb828945f070b18ee
Isaakavo <isaakhaas96@gmail.com>
2021-03-24 16:58:44 -0600

    getSpinnerMonths moved spinner class

--- app/src/main/java/UI/MonthSpinner.kt
+++ app/src/main/java/UI/MonthSpinner.kt
@@ -10,14 +10,11 @@ class MonthSpinner(private val expenseViewModel: ExpenseViewModel) : AdapterView
 
   override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
     val selectedItem = parent?.getItemAtPosition(position) as String
-    //If the user wants to see all the expenses
     if (selectedItem == "All Expenses") {
       expenseViewModel.setDesiredDate("All Expenses")
     } else {
-      //We only display the expenses of the desired month
       val selectedItemFormatter =
-          SimpleDateFormat("MMM yyyy", Locale.getDefault()).parse(selectedItem)!!
-      //We use a different format to make a db query
+        SimpleDateFormat("MMM yyyy", Locale.getDefault()).parse(selectedItem)!!
       val desiredDate = SimpleDateFormat("y-MM", Locale.getDefault()).format(selectedItemFormatter)
       expenseViewModel.setDesiredDate(desiredDate)
     }
@@ -27,4 +24,29 @@ class MonthSpinner(private val expenseViewModel: ExpenseViewModel) : AdapterView
     return
   }
 
+  companion object {
+    //Get the past month of the year
+    fun getSpinnerMonths(): ArrayList<String> {
+      val formatter = SimpleDateFormat("MMMM yyyy", Locale.getDefault())
+      val list = arrayListOf<String>()
+      Calendar.getInstance().let { calendar ->
+        val monthBefore = Calendar.getInstance()
+        val currentYear = calendar.get(Calendar.YEAR)
+        //We loop to know which month has already pass
+        for (i in 1 until 12) {
+          //We only want the month of current year
+          val pastYear = monthBefore.get(Calendar.YEAR)
+          if (pastYear == currentYear) {
+            //Format the month and add to array
+            list.add(formatter.format(calendar.timeInMillis))
+            //Go backwards to know which month has passed
+            calendar.add(Calendar.MONTH, -1)
+            monthBefore.add(Calendar.MONTH, -1)
+          }
+        }
+      }
+      //list.add("All Expenses")
+      return list
+    }
+  }
 }
