52eda8cacb3367eff57202334fed78334a295bd2
Isaakavo <isaakhaas96@gmail.com>
2021-03-30 12:33:26 -0600

    Add format to have day in date display

--- app/src/main/java/com/example/monthlyexpenses/Util.kt
+++ app/src/main/java/com/example/monthlyexpenses/Util.kt
@@ -1,5 +1,7 @@
 package com.example.monthlyexpenses
 
+import android.content.res.Resources
+import com.example.monthlyexpenses.data.Expenses
 import com.example.monthlyexpenses.data.Items
 import java.text.DateFormat
 import java.text.SimpleDateFormat
@@ -18,9 +20,13 @@ fun setDateFormat(timestamp: Long): String {
 
 fun setDateMonthFormatted(date: String): String {
   val selectedItemFormatter =
-    SimpleDateFormat("y-MM", Locale.getDefault()).parse(date)
+      SimpleDateFormat("y-MM", Locale.getDefault()).parse(date)
   val formatter = SimpleDateFormat("MMMM yyyy", Locale.getDefault())
   return formatter.format(selectedItemFormatter!!)
+}
+
+fun setDayMonthFormat(timestamp: Long): String {
+  return SimpleDateFormat("EEEE, d MMMM yyyy", Locale.getDefault()).format(timestamp)
 }
 
 
@@ -35,4 +41,12 @@ fun getTotals(itemList: List<Items>): Float {
     total += item.price.toFloat()
   }
   return total
+}
+
+fun concatenateString(expenses: Expenses, res: Resources, R: Int): String {
+  return res.getString(R, expenses.total.toString())
+}
+
+fun concatenateItemPrice(item: Items, res: Resources, R: Int): String {
+  return res.getString(R, item.price)
 }
